86cedd33e40cb34b5626ea6987e24502a305ecff
Seyoon Park <114111012+s3yoonpark@users.noreply.github.com>
2023-04-06 19:59:58 +0100

    2022 q3

---
+++ 2022/q3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+string solve(string n, long long int i) {
+		
+	return "ABCD"; 
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	string n; long long int i; 
+	cin >> n >> i; 
+	cout << solve(n, i); 
+	return 0; 
+}
